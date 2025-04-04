a00fc0f361bca04d4fa889501188c7087a83ade4
seetharampullela <seetharampullela@gmail.com>
2025-04-04 10:18:21 +0530

    Changes for product controller

--- nodejs-project-3-mern/backend/controller/product.controller.js
+++ nodejs-project-3-mern/backend/controller/product.controller.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import Product from "../../models/product.model.js";
+import Product from "../models/product.model.js";
 
 /* Find all products */
 export const getProduct = async (req, res) => {
